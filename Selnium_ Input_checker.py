7a29b4b8cb7b90aa206519bd14b424d96841e602
Teknical Mage <arcazjordan@gmail.com>
2020-09-29 22:11:50 -0400

    pusg2
    pusg2

--- Selnium_ Input_checker.py
+++ Selnium_ Input_checker.py
@@ -6,36 +6,7 @@ from selenium.webdriver.common.keys import Keys
 import pickle
 import tweepy
 
-
-consumer_key = ('')
-consumer_secret = ('')
-access_token = ('')
-access_token_secret = ('')
-
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
-
-api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
-
-
-
-
-def find():
-    print(1)
-    chrome_options = Options()
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--disable-extensions")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    driver = webdriver.Chrome(chrome_options=chrome_options)
-    f = f = open("nt.txt", "a")
-    driver.get('https://www.canlii.org/en/on/#search/type=decision&jId=on,unspecified&sort=decisionDateDesc&text=%22Employment%20Law%22&includeSccJudgments=true&origJId=on')
-    x = driver.find_element_by_xpath('//a[@data-result-index="1"]').get_attribute('href')
-    print(x)
-    z = str(x)
-    print(z)
-    f.write(z)
-    f.close()
-    
+ 
 def find_replace():
     print(1)
     chrome_options = Options()
@@ -55,7 +26,7 @@ def find_replace():
     prove()
 
 def prove():
-    f = f = open("nt.txt", "r")
+    f = open("nt.txt", "r")
     chrome_options = Options()
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-extensions")
@@ -77,9 +48,32 @@ def prove():
         print('no')
         find_replace()
 
+
+f = open("keyshere.txt","r")
+x1 = str(f.readline())
+x2 = str(f.readline())
+x3 = str(f.readline())
+x4 = str(f.readline())
+
+print(x1)
+print(x2)
+print(x3)
+print(x4)
+
+
+consumer_key = (x1)
+consumer_secret = (x2)
+access_token = (x3)
+access_token_secret = (x4)
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+
+
+
 while True:
-    try:
-        prove()
-        time.sleep(7200)
-    except:
-        pass
+    prove()
+    time.sleep(7200)
+ 
